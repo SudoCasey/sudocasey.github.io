9b996ae834e6221aa6bcfe761d9eb1504491e4e2
CaseyADASC <Casey@adasitecompliance.com>
2025-04-08 22:16:33 -0400

    Github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,10 @@
 const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    unoptimized: true, // Disable default image optimization
-  },
-  assetPrefix: isProd ? '/sudocasey.github.io/' : '',
-  basePath: isProd ? '/sudocasey.github.io' : '',
   output: 'export',
-  // Add configuration to handle dynamic routes
+  // Configure for GitHub Pages root URL
+  assetPrefix: isProd ? '' : '',
+  basePath: isProd ? '' : '',
   trailingSlash: true,
   // Disable image optimization for static export
   images: {
